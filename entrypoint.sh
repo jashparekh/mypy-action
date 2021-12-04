20e200760a66ce99185e4b6651bae9d96577705b
Jash Parekh <jparekh1@wayfair.com>
2021-12-04 15:34:37 -0500

    Initial config

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,3 @@ mypy_flags=${4:-""}
 
 # get mypy version
 mypy --version
-
-# run mypy, tee output to file
-# shellcheck disable=2086
-#mypy $2 --show-column-numbers --hide-error-context $1 | tee "${output_file}"
-#exit_code="${PIPESTATUS[0]}"
-#
-## analyze output
-#python /github.py "${output_file}"
-#
-#exit "$exit_code"
