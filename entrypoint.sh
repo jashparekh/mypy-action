06b3ca33e0844b1dac42d9af83b0fcca0a10f1a0
Jash Parekh <jparekh1@wayfair.com>
2021-12-04 16:03:54 -0500

    Try commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 set -ax
 
+args = ""
+
 # mypy output file
 output_file=/tmp/mypy.out
 
 lint_path=${1:-.}
 mypy_version=${2:-0.910}
-mypy_config_file=${3:-""}
-mypy_flags=${4:-""}
+mypy_config_file=$3
+flags=$4
 
 pip install mypy==${mypy_version}
 
 # get mypy version
 mypy --version
+
+# concat mypy config file if provided
+if [ -z "$mypy_config_file" ] then
+  args = "${args} --config-file=${mypy_config_file}"
+
+# concat mypy flags if provided
+if [ -z "$flags" ] then
+  args = "${args} ${flags}"
+
+# run mypy
+mypy --show-column-numbers --hide-error-context $flags
